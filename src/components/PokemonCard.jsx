ae2843f9e01860c6ef7f6609ba9a04f0e6cef2f1
CLENALIO <caroline.le.nalio@gmail.com>
2023-03-28 13:28:00 +0200

    fonction pokemoncard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,26 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];  
+
 function PokemonCard() {
-    return <figure>
-      <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>;
-  }
+  const pokemon = pokemonList[1];
   
-  export default PokemonCard;
+  return (
+    <figure>
+      {pokemon.imgSrc? <img src = {pokemon.imgSrc} alt= {pokemon.name}/> : <p>???</p>}
+      <figcaption> {pokemon.name} </figcaption>
+    </figure>
+  )
+}
+
+  
+export default PokemonCard;
+
+
