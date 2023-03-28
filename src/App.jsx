9b6168e1338cf59b334b2b58ea56ac2c84df3668
CLENALIO <caroline.le.nalio@gmail.com>
2023-03-28 16:32:57 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import './App.css'
 import PokemonCard from "./components/PokemonCard.jsx"
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];  
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard name ={pokemonList[0].name} imgSrc = {pokemonList[0].imgSrc}/>
     </div>
   )
 }
